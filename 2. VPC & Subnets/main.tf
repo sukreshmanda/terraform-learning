d771dab7a579df86d85ee7ff5e4c949bb8cad870
sukresh.thoughtworks <manda.sukresh@thoughtworks.com>
2022-08-28 11:00:21 +0530

    vpc/subnets

--- 2. VPC & Subnets/main.tf
+++ 2. VPC & Subnets/main.tf
@@ -2,16 +2,19 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_vpc" "my-vpc" {
+resource "aws_vpc" "first-vpc" {
     cidr_block = "10.0.0.0/16"
     tags = {
       "Name" = "production"
     }
 }
+resource "aws_vpc" "second-vpc" {
+    cidr_block = "10.1.0.0/16"
+}
 
 resource "aws_subnet" "subnet-1" {
-    vpc_id = aws_vpc.my-vpc.id
-    cidr_block = "10.0.1.0/24"
+    vpc_id = aws_vpc.second-vpc.id
+    cidr_block = "10.1.1.0/24"
 
     tags = {
         "Name": "prod-subnet-1"
