17ba3f079878b58a7a8c5879b1622da586d8cb4b
sukresh.thoughtworks <manda.sukresh@thoughtworks.com>
2022-08-29 11:13:21 +0530

    public subnet to access from internet

---
+++ 3. Public Subnet/main.tf
@@ -0,0 +1,41 @@
+# provider
+provider "aws" {
+    region = "ap-south-1"
+}
+
+# public vpc
+resource "aws_vpc" "public-vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      "Name" = "production-public-vpc"
+    }
+}
+
+# internet gateway for public access
+resource "aws_internet_gateway" "production-ig" {
+    vpc_id = aws_vpc.public-vpc.id
+    tags = {
+      "Name" = "production-ig"
+    }
+}
+
+# add public route to default route table for vpc
+resource "aws_default_route_table" "public-vpc-route-table" {
+    default_route_table_id = aws_vpc.public-vpc.default_route_table_id
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.production-ig.id
+    } 
+}
+
+# public subnet in public vpc
+resource "aws_subnet" "production-public-subnet" {
+    vpc_id = aws_vpc.public-vpc.id
+    cidr_block = "10.0.0.0/16"
+    map_public_ip_on_launch = true
+    tags = {
+      "Name" = "production-public-subnet"
+    }
+}
+
+# public aws instance
